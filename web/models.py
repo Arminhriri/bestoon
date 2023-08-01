601f9fd2a028ec8977113bdb2ce0edf63d1b0640
armin <arminhariri@yahoo.com>
2023-08-01 23:33:10 +0330

    Income model is here.

--- web/models.py
+++ web/models.py
@@ -1,10 +1,18 @@
-from django.db import models
+import sys
+print(sys.path)
+
 from django.contrib.auth.models import User
-from django.conf import settings
+from django.db import models
 
 # Create your models here.
 
 class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+
+class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
